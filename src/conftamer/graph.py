051aebf47a72f16b151ee5158f95c87ae47918f8
Patrick Lau <patricklyc@berkeley.edu>
2026-08-16 22:02:40 -0700

    affix labels

--- src/conftamer/graph.py
+++ src/conftamer/graph.py
@@ -6,20 +6,19 @@ from conftamer.csv import read_csv
 from conftamer.models import BaseNode
 
 
-def to_vertices(edges: list[tuple]) -> set[BaseNode]:
-    vs = set(itertools.chain.from_iterable(edges))
-    return vs
-
-
-def to_ig_edges(edges: list[tuple]):
-    ig_edges = [(str(a), str(b)) for a, b in edges]
-    return ig_edges
-
-
 def to_graph(edges: list[tuple[BaseNode, BaseNode]]) -> ig.Graph:
     vs = list(set(itertools.chain.from_iterable(edges)))
     ig_edges = [(vs.index(a), vs.index(b)) for a, b in edges]
-    vattrs = [v.model_dump() for v in vs]
+    vattrs: list[dict[str, str]] = [v.model_dump() for v in vs]
+    for v in vattrs:
+        v["label"] = f"{v.get('module_id')} {
+            (
+                v.get('param_name')
+                or v.get('request_id')
+                or v.get('request_pattern')
+            )
+        }"
+
     g = ig.Graph(len(vs), ig_edges, directed=True)
     for i in range(len(vs)):
         g.vs[i].update_attributes(vattrs[i])
@@ -27,8 +26,8 @@ def to_graph(edges: list[tuple[BaseNode, BaseNode]]) -> ig.Graph:
 
 
 if __name__ == "__main__":
-    edges = read_csv("test_log.csv")
+    edges = read_csv("test_gen.csv")
 
     g = to_graph(edges)
     print(g)
-    g.write_graphml("test_log.graphml")
+    g.write_graphml("test_gen.graphml")
